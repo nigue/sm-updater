d2315ec7b9d4f36a8e0a799abc22d022d0b2a878
nigue <anibaldavor@gmail.com>
2024-07-24 18:06:29 -0400

    feat: update sql

--- ext/supabase.sql
+++ ext/supabase.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS public.sm_configuration;
 DROP TABLE IF EXISTS public.sm_arcade_paths;
 DROP TABLE IF EXISTS public.sm_arcade_credentials;
 drop function if exists publish_report(configuration_name, message)
-drop function if exists update_pack(configuration_name, pack_name, pack_identifier)
+drop function if exists update_pack(configuration_name, pack_name, new_pack_identifier)
 
 -- Structure and contraints
 create table
